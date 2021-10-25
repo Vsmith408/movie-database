43981dc628b288d955c8ca4fc0980a3771946f08
Victoria Smith <victoriasmithh428@gmail.com>
2021-10-25 19:33:39 -0400

    toggle background & cancel btn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,23 @@
 const addMovieModal = document.getElementById("add-modal");
 const startMovieBtn = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
 };
 
 startMovieBtn.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovie);
